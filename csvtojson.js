a58aefb24b02ca54294f1d8a3f32f41cfa7e5e4f
spoorthi <spoorthi@csgkarnataka.in>
2024-04-25 17:20:32 +0530

    storing csv questions to mongodb

--- csvtojson.js
+++ csvtojson.js
@@ -1,3 +1,16 @@
+
 let csvToJson = require('convert-csv-to-json');
-csvToJson.formatValueByType()
-.getJsonFromCsv(questions.csv);
+const {q10Schema} = require('./config.js');
+
+const csvFilePath = './questions.csv';
+const json = csvToJson.fieldDelimiter(',').getJsonFromCsv(csvFilePath)
+console.log(json);
+q10Schema.insertMany(json)
+  .then((result) => {
+    console.log('Inserted documents successfully:', result);
+    // Optionally, close the database connection if needed
+  })
+  .catch((err) => {
+    console.error('Failed to insert documents:', err);
+    // Optionally, handle the error
+  });

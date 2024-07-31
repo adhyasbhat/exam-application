f60aaf46faa80cfe0513a88ae274958ca0ad05ad
adhyasbhat <spoorthi@csgkarnataka.in>
2024-07-31 13:06:03 +0530

    otp verification

--- src/Controllers/candidatescontrollers.js
+++ src/Controllers/candidatescontrollers.js
@@ -92,7 +92,18 @@ candidateController.updateCandidate = async (req, res) => {
         res.status(500).send("Error updating candidate: " + error.message);
     }
 };
-
+candidateController.verifyOTP = async (req, res) => {
+    try {
+        if (req.body.otp === "1234") {
+            res.status(200).send("OTP verified successfully");
+        } else {
+            res.status(401).send("Incorrect OTP");
+        }
+    } catch (error) {
+        console.error("Error verifying OTP:", error);
+        res.status(500).send("Error verifying OTP: " + error.message);
+    }
+};
 candidateController.loginCandidate = async (req, res) => {
     try {
         const candidate = await Candidate.findOne({ email: req.body.email });
--- src/Routes/candidatesroutes.js
+++ src/Routes/candidatesroutes.js
@@ -6,5 +6,6 @@ router.post("/registerCandidate", candidateController.registerCandidate);
 router.post("/loginCandidate", candidateController.loginCandidate);
 router.put("/updateCandidate", candidateController.updateCandidate);
 router.post("/sendOTP", candidateController.sendOTP);
+router.post("/verifyOTP", candidateController.verifyOTP);
 
 module.exports = router;
